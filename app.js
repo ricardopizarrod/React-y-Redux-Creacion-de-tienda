b8ac41ddd10478d0e04a2ffbc0be0e013a15104d
Ricardo Pizarro <ricardopizarro@MacBook-Pro.local>
2020-02-10 16:47:38 -0300

    Episodio 4 Creación de action y dispatch

---
+++ app.js
@@ -0,0 +1,51 @@
+//import { createStore } from "redux"; const createStore = requiere('redux').createStore;
+const createStore = requiere('redux').createStore;
+
+//##### Actions
+const BUY_POKEMON = 'BUY_POKEMON';
+const BUY_POKEMON = 'RETURN_POKEMON';
+
+const buy_pokemon_action = (cant) => {
+    return {
+        type: BUY_POKEMON,
+        payload: cant
+    }
+}
+
+const return_pokemon_action = (cant) =>{
+    return {
+        type: RETURN_POKEMON,
+        payload: cant
+    }
+}
+
+
+//##### Reducers
+const default_games_state = {
+    pokemon: 10
+}
+const games_reducer = (state = default_games_state, action) => {
+    switch(action.type){
+        case BUY_POKEMON: {
+            return {
+                pokemon: state.pokemon - action.payload
+            }
+        }
+        case RETURN_POKEMON: {
+            return {
+                pokemon: state.pokemon + action.payload
+            }
+        }
+        default:return state;
+    
+    } 
+}
+
+//##### Store
+const store = createStore(games_reducer);
+console.log('Estado inicial: ', store.getState());
+store.subscribe(() => {
+    console.log('Cambio de estado: ', store.getState());
+});
+store.dispatch(buy_pokemon_action(3));
+store.dispatch(return_pokemon_action(2));
