4f4dfd81a8768eedeb68d3a83a5b9a0b320f274f
Ricardo Pizarro <ricardopizarro@MacBook-Pro.local>
2020-02-10 22:26:43 -0300

    EP.6 Creando multiples reducers y los combinamos

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 //import { createStore } from "redux"; const createStore = requiere('redux').createStore;
 const createStore = require('redux').createStore;
-
+const combineReducers = require('redux').combineReducers;
 //##### Actions
 const BUY_POKEMON = 'BUY_POKEMON';
 const RETURN_POKEMON = 'RETURN_POKEMON';
+const BUY_YOSHI = 'BUY_YOSHI';
+const RETURN_YOSHI = 'RETURN_YOSHI';
+
 
 const buy_pokemon_action = (cant) => {
     return {
@@ -18,34 +21,66 @@ const return_pokemon_action = (cant) =>{
         payload: cant
     }
 }
-
-
+const BUY_SWITCH = 'BUY_SWITCH';
+const buy_switch_action = (cant) => {
+    return {
+        type: BUY_SWITCH,
+        payload: cant
+    }
+}
 //##### Reducers
 const default_games_state = {
-    pokemon: 10
+    pokemon: 10,
+    yoshi: 10
 }
 const games_reducer = (state = default_games_state, action) => {
     switch(action.type){
         case BUY_POKEMON: {
             return {
+                ...state,
                 pokemon: state.pokemon - action.payload
             }
         }
         case RETURN_POKEMON: {
             return {
+                ...state,
                 pokemon: state.pokemon + action.payload
             }
         }
-        default:return state;
-    
+        default: return state;
     } 
 }
 
+const default_consoles_state = {
+    ps5: 30,
+    switch: 30
+
+}
+
+const consoles_reducer = (state = default_consoles_state, action) => {
+    switch(action.type){
+        case BUY_SWITCH: {
+            return {
+                ...state,
+                switch: state.switch - action.payload
+            }
+        }
+        default: return state;
+    } 
+}
+
+const rootReducers = combineReducers({
+    games_reducer,
+    consoles_reducer
+});
+
 //##### Store
-const store = createStore(games_reducer);
+const store = createStore(rootReducers);
 console.log('Estado inicial: ', store.getState());
-store.subscribe(() => {
+store.subscribe(() => { 
     console.log('Cambio de estado: ', store.getState());
 });
-store.dispatch(buy_pokemon_action(3));
-store.dispatch(return_pokemon_action(2));
+//store.dispatch(buy_pokemon_action(3));
+//store.dispatch(return_pokemon_action(2)); */
+
+store.dispatch(buy_switch_action(2));
